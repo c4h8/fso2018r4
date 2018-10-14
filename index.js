5b74d637bdb1ed7d93637e3dbf0d87f471813345
Panu Liesiö <panu.liesio@aalto.fi>
2018-10-14 13:53:17 +0300

    complete assigments 4.1 and 4.2

--- index.js
+++ index.js
@@ -5,14 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-const Blog = mongoose.model('Blog', {
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-});
-
-module.exports = Blog;
+const blogsRouter = require('./controllers/blogs');
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -25,23 +18,7 @@ const url = process.env.MONGODB_URI;
 
 mongoose.connect(url);
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs);
-    });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result);
-    });
-});
+app.use('/api/blogs', blogsRouter);
 
 const PORT = 3003;
 app.listen(PORT, () => {
